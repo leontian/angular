d2dd0f39df6927ec7c4123175eac3dc54550a33b
leontian <l.tian.email@gmail.com>
2015-07-13 18:40:59 -0400

    initial commit, beginning node

--- app.js
+++ app.js
@@ -1,14 +1,59 @@
-var app = angular.module('angular', []);
+var app = angular.module('angular', ['ui.router']);
 
-app.controlller('MainCtrl, [
-                '$scope',
-                function($scope){
-                        $scope.test = 'Hello world!';
-                        $scope.posts = [
-                           'post 1',
-                           'post 2',
-                           'post 3',
-                           'post 4',
-                           'post 5',
-                           ];
-                }]);
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+    $stateProvider
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      });
+    
+    $urlRouterProvider.otherwise('home');
+  }]);
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function ($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+  }]);
+
+app.controller('MainCtrl', [
+  '$scope',
+  'posts',
+  function ($scope, posts) {
+    $scope.posts = posts.posts;
+    $scope.addPost = function () {
+      if (!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+    $scope.incrementUpvotes = function (post) {
+      post.upvotes += 1;
+    };
+  }]);
+
+app.factory('posts', [function () {
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
